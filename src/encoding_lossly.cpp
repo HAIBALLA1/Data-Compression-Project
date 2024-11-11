b1c606b3a41adb30c984f8956c62d8073a8f596c
farouk <haiballaelvarough@gmail.com>
2024-11-11 17:33:25 +0100

    implementation de la fonction encode

--- src/encoding_lossly.cpp
+++ src/encoding_lossly.cpp
@@ -1,3 +1,19 @@
 //
 // Created by ing on 08/11/24.
 //
+
+#include <iostream>
+#include "encoding_lossly.h"
+
+int main() {
+    using namespace encoding::lossly;
+
+    std::vector<int>source ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    std::size_t nb_coefs=4;
+    std::vector<double> resu=DiscreteCosinus::encode<int, double>(source, nb_coefs);;
+    std::cout << "Résultat de l'encodage :" << std::endl;
+    for (std::size_t i = 0; i < resu.size(); ++i) {
+        std::cout << "Coefficient " << i << " : " << resu[i] << std::endl;
+    }
+    return 0;
+}
--- src/encoding_lossly.h
+++ src/encoding_lossly.h
@@ -1,7 +1,31 @@
-//
+    //
 // Created by ing on 08/11/24.
 //
+
+#include <vector>
+#include <cmath>
+
 namespace encoding::lossly {
+
+    struct DiscreteCosinus {
+        template<typename D, typename R>
+        static std::vector<R> encode(std::vector<D> const & source, std::size_t const & nb_coefs) {
+            std::vector<R> res;
+
+            if (source.empty()) {
+                return res;
+
+            }
+            for(std::size_t i = 0; i < nb_coefs; ++i) {
+                res.push_back(source[0]);
+                for (std::size_t j = 1; j < source.size(); ++j) {
+                    res[i] = (res[i] + source[j] * std::cos(2 * M_PI * i * j / nb_coefs));
+                }
+            }
+            return res;
+        }
+
+    };
 
 
 };
